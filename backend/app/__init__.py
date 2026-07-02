7853eeee056f467a8c68e39f39e3c4810b0ee452
Aayush Sharma <bhattsanchit4@gmail.com>
2026-07-02 13:06:47 +0530

    ..

--- backend/app/__init__.py
+++
@@ -1,1 +0,0 @@
-"""GenCloudNet backend package."""
--- ml/gencloudnet_ml/__init__.py
+++
@@ -1,5 +0,0 @@
-"""GenCloudNet research and inference package."""
-
-from .pipeline import PipelineArtifacts, PipelineConfig, PipelineEngine
-
-__all__ = ["PipelineArtifacts", "PipelineConfig", "PipelineEngine"]
